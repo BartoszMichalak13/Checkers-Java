1ebd44e7b96db1d62a8708554a6442a197f74318
kf0r <krzysfor@gmail.com>
2022-12-16 10:10:59 +0100

    proba tworzenia planszy

--- src/main/java/org/example/Plansza.java
+++ src/main/java/org/example/Plansza.java
@@ -6,13 +6,41 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-public abstract class Plansza {
-    private int size;
+public class Plansza implements MouseListener{
+    private final int size;
+    private boolean isWhiteTurn;
 
+    public Plansza(int size, boolean isPolish) {
+        this.size = size;
+        this.isWhiteTurn = true;
 
-    private void ustawPionki(){
+        if(size==8){
+
+                for (int i = 0; i < 8; i++) {
+                    for (int j = 0; j < 8; j++) {
+                        if (((i == 0 || i == 2) && j % 2 == 1) || (i == 1 && j % 2 == 0)) {
+                            new Pionek(i, j, true, isPolish, this);
+                        } else if (((i == 7 || i == 5) && j % 2 == 0) || (i == 6 && j % 2 == 1)) {
+                            new Pionek(i, j, false, isPolish, this);
+                        }
+                    }
+                }
+
+        }else if(size==10){
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    if (((i == 0 || i == 2 ) && j % 2 == 1) || ((i == 1||i==3) && j % 2 == 0)) {
+                        new Pionek(i, j, true, isPolish, this);
+                    } else if (((i == 9 || i == 7) && j % 2 == 1) || ((i == 8 ||i==6)&& j % 2 == 0)) {
+                        new Pionek(i, j, false, isPolish, this);
+                    }
+                }
+            }
+        }
 
     }
+
+
 
     public int getSize() {
         return size;
@@ -20,5 +48,40 @@ public abstract class Plansza {
 
     public void setSize(int size) {
         this.size = size;
+    }
+
+    public boolean isWhiteTurn() {
+        return isWhiteTurn;
+    }
+
+    public void setWhiteTurn(boolean whiteTurn) {
+        isWhiteTurn = whiteTurn;
+    }
+
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
