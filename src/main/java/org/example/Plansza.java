614cb63f9a0ece21de2750facf62dcef5c8e2824
kf0r <krzysfor@gmail.com>
2022-12-16 10:54:46 +0100

    dalsze tworzenie planszy

--- src/main/java/org/example/Plansza.java
+++ src/main/java/org/example/Plansza.java
@@ -11,33 +11,38 @@ public class Plansza implements MouseListener{
     private boolean isWhiteTurn;
 
     public Plansza(int size, boolean isPolish) {
-        this.size = size;
+        this.size = 8;
         this.isWhiteTurn = true;
-
         if(size==8){
-
-                for (int i = 0; i < 8; i++) {
-                    for (int j = 0; j < 8; j++) {
-                        if (((i == 0 || i == 2) && j % 2 == 1) || (i == 1 && j % 2 == 0)) {
-                            new Pionek(i, j, true, isPolish, this);
-                        } else if (((i == 7 || i == 5) && j % 2 == 0) || (i == 6 && j % 2 == 1)) {
-                            new Pionek(i, j, false, isPolish, this);
-                        }
-                    }
-                }
-
-        }else if(size==10){
-            for (int i = 0; i < 10; i++) {
-                for (int j = 0; j < 10; j++) {
-                    if (((i == 0 || i == 2 ) && j % 2 == 1) || ((i == 1||i==3) && j % 2 == 0)) {
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    if (((i == 0 || i == 2) && j % 2 == 1) || (i == 1 && j % 2 == 0)) {
                         new Pionek(i, j, true, isPolish, this);
-                    } else if (((i == 9 || i == 7) && j % 2 == 1) || ((i == 8 ||i==6)&& j % 2 == 0)) {
+                    } else if (((i == 7 || i == 5) && j % 2 == 0) || (i == 6 && j % 2 == 1)) {
                         new Pionek(i, j, false, isPolish, this);
                     }
                 }
             }
         }
+        else if(size==10){
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    if (((i == 0 || i == 2 ) && j % 2 == 1) || ((i == 1||i==3) && j % 2 == 0)) {
+                        new Pionek(i, j, true, isPolish, this);
+                    } else if (((i == 9 || i == 7) && j % 2 == 0) || ((i == 8 ||i==6)&& j % 2 == 1)) {
+                        new Pionek(i, j, false, isPolish, this);
+                    }
+                }
+            }
+        }
+    }
 
+    JPanel jPanel = new JPanel(){
+        public void paint(){
+            size_of_window[0] = jFrame.getContentPane().getSize();
+            windowH[0] = (int) (size_of_window[0].getHeight() / 8);
+            windowW[0] = (int) (size_of_window[0].getWidth() / 8);
+        }
     }
 
 
@@ -46,19 +51,9 @@ public class Plansza implements MouseListener{
         return size;
     }
 
-    public void setSize(int size) {
-        this.size = size;
-    }
-
     public boolean isWhiteTurn() {
         return isWhiteTurn;
     }
-
-    public void setWhiteTurn(boolean whiteTurn) {
-        isWhiteTurn = whiteTurn;
-    }
-
-
 
     @Override
     public void mouseClicked(MouseEvent e) {
