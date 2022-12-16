3cd8b1a9fb5aaa530ece0d127a1110bc7ebcbfc2
kf0r <krzysfor@gmail.com>
2022-12-16 02:29:41 +0100

    dodadnie flag typu gry do pionkow, pomoc w przyszlym korzystaniu z buildera. kazdy pionek tez ma plansze, ktorej zna rozmiar.

--- src/main/java/org/example/Pionek.java
+++ src/main/java/org/example/Pionek.java
@@ -6,20 +6,26 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public abstract class Pionek extends MouseListener {
+public class Pionek {
     private int x;
-    prvate int y;
+    private int y;
     private boolean isWhite;
     private boolean isQueen;
     private boolean isActive;
+    private final boolean isPolish;
+    private final Plansza plansza;
+    private int sizeOfPlansza;
     private static ArrayList<Pionek> pionki = new ArrayList<>();
 
-    public Pionek(int x, int y, boolean isWhite) {
+    public Pionek(int x, int y, boolean isWhite, boolean isPolish, Plansza plansza) {
         this.x = x;
         this.y = y;
         this.isWhite = isWhite;
         this.isQueen = false;
         this.isActive = false;
+        this.isPolish=isPolish;
+        this.plansza = plansza;
+        this.sizeOfPlansza = plansza.getSize();
         pionki.add(this);
     }
 
@@ -55,12 +61,20 @@ public abstract class Pionek extends MouseListener {
         isQueen = queen;
     }
 
+    public boolean isPolish(){
+        return isPolish;
+    }
+
     public boolean isActive() {
         return isActive;
     }
 
     public void setActive(boolean active) {
         isActive = active;
+    }
+
+    public Plansza getPlansza() {
+        return plansza;
     }
 
     public static ArrayList<Pionek> getPionki() {
@@ -70,6 +84,12 @@ public abstract class Pionek extends MouseListener {
     public void przesun(int x, int y){
         this.x=x;
         this.y=y;
+
+        if(this.isWhite()&&y==this.sizeOfPlansza-1){
+            this.setQueen(true);
+        }else if((!this.isWhite())&&y==0){
+            this.setQueen(true);
+        }
     }
 
     public static Pionek getPionekByCords(int x, int y) {
@@ -102,19 +122,54 @@ public abstract class Pionek extends MouseListener {
         return pion;
     }
 
-    public static ArrayList<Integer> bicia(int x, int y, boolean isWhite, boolean isQueen) {
+    //TODO: ogarnięcie bić damką, w trybie gry polskim
+
+    private static ArrayList<Integer> biciaDamka(int x, int y, boolean isWhite, boolean isPolish){
+        ArrayList<Integer> xandy = new ArrayList<>();
+        if(isPolish){
+
+        }else{
+            for(Pionek pion:pionki){
+                if(isWhite!=pion.isWhite()){
+                    if(pion.getX()==x+1&&pion.getY()==y+1&&
+                            getPionekByCords(x+2, y+2)==null&&
+                            x+2<pion.sizeOfPlansza&&y+2< pion.sizeOfPlansza){
+                        xandy.add(x+2, y+2);
+                    }
+                    if(pion.getX()==x+1&&pion.getY()==y-1&&
+                            getPionekByCords(x+2, y-2)==null
+                            &&x+2<pion.sizeOfPlansza&&y-2>=0){
+                        xandy.add(x+2, y-2);
+                    }
+                    if(pion.getX()==x-1&&pion.getY()==y+1 &&
+                            getPionekByCords(x-2, y+2)==null&&
+                            x-2>=0&&y+2< pion.sizeOfPlansza){
+                        xandy.add(x-2, y+2);
+                    }
+                    if(pion.getX()==x-1&&pion.getY()==y-1&&
+                            getPionekByCords(x-2, y-2)==null&&
+                            x-2>=0&&y-2>=0){
+                        xandy.add(x-2, y+2);
+                    }
+                }
+            }
+        }
+        return xandy;
+    }
+
+    public static ArrayList<Integer> bicia(int x, int y, boolean isWhite, boolean isQueen, boolean isPolish) {
         ArrayList<Integer> xandy = new ArrayList();
         if (!isQueen) {
-            int i
+            int i;
             if (isWhite) {
                 for(i = 0; i < pionki.size(); ++i) {
                     if (!((Pionek)pionki.get(i)).isWhite && ((Pionek)pionki.get(i)).getY() == y + 1) {
                         if (((Pionek)pionki.get(i)).getX() == x + 1) {
-                            if (getPionekByCords(x + 2, y + 2) == null && x + 2 < 8 && y + 2 < 8) {
+                            if (getPionekByCords(x + 2, y + 2) == null && x + 2 < pionki.get(i).sizeOfPlansza && y + 2 < pionki.get(i).sizeOfPlansza) {
                                 xandy.add(x + 2);
                                 xandy.add(y + 2);
                             }
-                        } else if (((Pionek)pionki.get(i)).getX() == x - 1 && getPionekByCords(x - 2, y + 2) == null && x - 2 >= 0 && y + 2 < 8) {
+                        } else if (((Pionek)pionki.get(i)).getX() == x - 1 && getPionekByCords(x - 2, y + 2) == null && x - 2 >= 0 && y + 2 < pionki.get(i).sizeOfPlansza) {
                             xandy.add(x - 2);
                             xandy.add(y + 2);
                         }
@@ -124,7 +179,7 @@ public abstract class Pionek extends MouseListener {
                 for(i = 0; i < pionki.size(); ++i) {
                     if (((Pionek)pionki.get(i)).isWhite && ((Pionek)pionki.get(i)).getY() == y - 1) {
                         if (((Pionek)pionki.get(i)).getX() == x + 1) {
-                            if (getPionekByCords(x + 2, y - 2) == null && x + 2 < 8 && y - 2 >=0) {
+                            if (getPionekByCords(x + 2, y - 2) == null && x + 2 < pionki.get(i).sizeOfPlansza && y - 2 >=0) {
                                 xandy.add(x + 2);
                                 xandy.add(y - 2);
                             }
@@ -136,12 +191,67 @@ public abstract class Pionek extends MouseListener {
                 }
             }
         }else{
-            return biciaDamka();
+            return biciaDamka(x, y, isWhite, isPolish);
         }
         return xandy;
     }
 
-    private ArrayList<Integer> biciaDamka(){
+    //TODO: ogarniecie ruchów damka w trybie gry polskim.
 
+    public ArrayList<Integer> ruchyDamka(boolean isPolish){
+        ArrayList<Integer> xandy = new ArrayList<>();
+        if(isPolish){
+
+        }else{
+            if(getPionekByCords(x+1, y+1)==null&&x+1<this.sizeOfPlansza&&y+1<this.sizeOfPlansza){
+                xandy.add(x+1, y+1);
+            }
+            if(getPionekByCords(x+1, y-1)==null&&y-1>=0&&x+1<this.sizeOfPlansza){
+                xandy.add(x+1, y-1);
+            }
+            if(getPionekByCords(x-1, y+1)==null&&y+1<this.sizeOfPlansza&&x-1>=0){
+                xandy.add(x-1, y+1);
+            }
+            if(getPionekByCords(x-1, y-1)==null&&y-1>=0&&x-1>=0){
+                xandy.add(x-1, y-1);
+            }
+        }
+        return xandy;
+    }
+
+    public ArrayList<Integer> legalneKafelki(){
+        ArrayList<Integer> legalneKafelki = new ArrayList<>();
+        if(Pionek.bicia(this.getX(), this.getY(), this.isWhite(), this.isQueen(), this.isPolish()).isEmpty()){
+            if (this.isQueen()){
+                return this.ruchyDamka(isPolish);
+            }else{
+                if(this.isWhite()){
+                    if((getPionekByCords(this.getX()+1, this.getY()+1)==null)&&(this.getX()+1<8&&this.getY()+1<8)){
+                        legalneKafelki.add(this.getX()+1);
+                        legalneKafelki.add(this.getY()+1);
+                    }
+                    if((getPionekByCords(this.getX()-1, this.getY()+1)==null)&&(this.getX()-1>=0&&this.getY()+1<8)){
+                        legalneKafelki.add(this.getX()-1);
+                        legalneKafelki.add(this.getY()+1);
+                    }
+                }else if(!this.isWhite()){
+                    if((getPionekByCords(this.getX()+1, this.getY()-1)==null)&&(this.getX()+1<8&&this.getY()-1>=0)){
+                        legalneKafelki.add(this.getX()+1);
+                        legalneKafelki.add(this.getY()-1);
+                    }
+                    if((getPionekByCords(this.getX()-1, this.getY()-1)==null)&&(this.getX()-1>=0&&this.getY()-1>=0)){
+                        legalneKafelki.add(this.getX()-1);
+                        legalneKafelki.add(this.getY()-1);
+                    }
+                }
+            }
+        }else{
+            if(!this.isQueen()) {
+                return Pionek.bicia(this.getX(), this.getY(), this.isWhite(), this.isQueen(), this.isPolish());
+            }else{
+                return Pionek.biciaDamka(this.getX(), this.getY(), this.isWhite(), this.isPolish());
+            }
+        }
+        return legalneKafelki;
     }
 }
--- src/main/java/org/example/Plansza.java
+++ src/main/java/org/example/Plansza.java
@@ -6,47 +6,19 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-public class Plansza {
-    JFrame jFrame = new JFrame();
-    int windowH;
-    int windowW;
-    Dimension size_of_window;
-    public Plansza() {
+public abstract class Plansza {
+    private int size;
+
+
+    private void ustawPionki(){
+
     }
-    public JFrame getFrame()
-    {
-        return this.jFrame;
+
+    public int getSize() {
+        return size;
     }
-    public Dimension rozmiar() {return this.jFrame.getContentPane().getSize();}
-    public int getwindowW(){return this.windowW;}
-    public int getwindowH(){return this.windowH;}
-    public void boardbuilder() {
 
-        jFrame.setBounds(10, 10, 1024, 720);
-        size_of_window = jFrame.getContentPane().getSize();
-        windowH = (int) (size_of_window.getHeight() / 8);
-        windowW = (int) (size_of_window.getWidth() / 8);
-        //Ustawiam pionki
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                if (((i == 0 || i == 2) && j % 2 == 1) || (i == 1 && j % 2 == 0)) {
-                    new Pionek(true, j, i);
-                } else if (((i == 7 || i == 5) && j % 2 == 0) || (i == 6 && j % 2 == 1)) {
-                    new Pionek(false, j, i);
-                }
-            }
-        }
-
-        //tworzę szachownice, myśle że to wszystko trzeba będzie dać do osobnych klas, ale narazie
-        //jestem w programistycznym cugu i robie cokolwiek byle działało, potem to sie ogarnie.
-
-        //GridLayout gridLayout = new GridLayout(0,1);
-        //gridLayout.add(jPanel);
-        jFrame.setPreferredSize(new Dimension(1024, 720));
-        jFrame.pack();
-        //jFrame.setLayout(gridLayout);
-        jFrame.add(new Pole());
-        jFrame.setDefaultCloseOperation(3);
-        jFrame.setVisible(true);
+    public void setSize(int size) {
+        this.size = size;
     }
 }
